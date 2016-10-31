3d1a78609359d2945e4fcc789100f4cf227836a0
Ben Boule <bboule@blackducksoftware.com>
2016-10-31 11:10:48 -0400

    Fix curl command

--- hub_curl.sh
+++ hub_curl.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -X POST -d @./build/reports/blackduck/bd-ci-test-repo_bdio.jsonld -H 'Content-Type:application/ld+json' "https://eng-hub.blackducksoftware.com/githubint/travis-scan?repoSlug=$TRAVIS_REPO_SLUG&branch=$TRAVIS_BRANCH&pullRequest=$TRAVIS_PULL_REQUEST"
+curl -v -X POST -d @./build/reports/blackduck/bd-ci-test-repo_bdio.jsonld -H 'Content-Type:application/ld+json' "https://eng-hub.blackducksoftware.com/githubint/travis-scan?repoSlug=$TRAVIS_REPO_SLUG&branch=$TRAVIS_BRANCH&pullRequest=$TRAVIS_PULL_REQUEST"
